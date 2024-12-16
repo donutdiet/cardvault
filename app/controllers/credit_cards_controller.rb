64a1dc902fcde08255b6199fa78228a0e4d3e379
Jerry Wang <jerrywang0404@gmail.com>
2024-12-15 23:01:02 -0500

    Implement basic CRUD functionality

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,5 +1,48 @@
 class CreditCardsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+
   def index
     @credit_cards = CreditCard.all
   end
+
+  def show
+  end
+
+  def new
+    @credit_card = CreditCard.new
+  end
+
+  def create
+    @credit_card = CreditCard.new(credit_card_params)
+    if @credit_card.save
+      redirect_to @credit_card
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @credit_card.update(credit_card_params)
+      redirect_to @credit_card
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @credit_card.destroy
+    redirect_to credit_cards_path
+  end
+
+  private
+    def set_product
+      @credit_card = CreditCard.find(params[:id])
+    end
+
+    def credit_card_params
+      params.expect(credit_card: [ :name ])
+    end
 end
